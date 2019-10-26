3eb4e49f215e8666bfa69e158d8e438634f37ed3
Iipal <malkoleyplay@gmail.com>
2019-10-26 12:28:49 +0300

    __nonnull

--- includes/wu_algo.h
+++ includes/wu_algo.h
@@ -6,7 +6,7 @@
 /*   By: tmaluh <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 18:17:12 by tmaluh            #+#    #+#             */
-/*   Updated: 2019/08/24 16:49:50 by tmaluh           ###   ########.fr       */
+/*   Updated: 2019/10/26 12:22:52 by tmaluh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@
 
 void	wu_draw_line(__v2df xy0,
 					__v2df xy1,
-					SDL_Surface *const surf) __nonnull((3));
+					SDL_Surface *const surf) NON_NULL((3));
 
 #endif
